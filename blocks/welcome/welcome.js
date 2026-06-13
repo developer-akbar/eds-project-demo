f21444b7f8fb6a05e67d6c5bfcf19d0bc7e976ba
developer-akbar <akbar.ak219@gmail.com>
2026-06-13 23:33:54 +0530

    Added new block

---
+++ blocks/welcome/welcome.js
@@ -0,0 +1,8 @@
+﻿export default function decorate(block) {
+  block.innerHTML = `
+    <div class="welcome-container">
+      <h2>Welcome to EDS KT Session</h2>
+      <p>My first custom block</p>
+    </div>
+  `;
+}
