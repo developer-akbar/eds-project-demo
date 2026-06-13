91bc241d1031fec92e9711c58c96c59bacb49276
developer-akbar <akbar.ak219@gmail.com>
2026-06-13 23:36:47 +0530

    Updated script to render DOM

--- blocks/welcome/welcome.js
+++ blocks/welcome/welcome.js
@@ -1,8 +1,18 @@
-﻿export default function decorate(block) {
-  block.innerHTML = `
-    <div class="welcome-container">
-      <h2>Welcome to EDS KT Session</h2>
-      <p>My first custom block</p>
-    </div>
-  `;
+﻿/**
+ * loads and decorates the welcome block
+ * @param {Element} block The block element
+ */
+export default function decorate(block) {
+  const container = document.createElement('div');
+  container.className = 'welcome-container';
+
+  const heading = document.createElement('h2');
+  heading.textContent = 'Welcome to EDS KT Session';
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'My first custom block';
+
+  container.appendChild(heading);
+  container.appendChild(paragraph);
+  block.appendChild(container);
 }
